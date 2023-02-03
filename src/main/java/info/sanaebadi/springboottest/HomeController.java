efcc4aeec4b6bc1dd5fe97b88a1bd7480213676e
sanaebadi <sana@journeyhero.net>
2023-02-03 13:42:59 +0200

    wrote a test

---
+++ src/main/java/info/sanaebadi/springboottest/HomeController.java
@@ -0,0 +1,13 @@
+package info.sanaebadi.springboottest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Home";
+    }
+}
