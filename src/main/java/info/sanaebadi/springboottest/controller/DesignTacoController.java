dd21e431dc9dc18297fc45392118d836b77c5cb1
sanaebadi <sana@journeyhero.net>
2023-02-14 11:55:16 +0200

    created a DesignTacoController and a convertor

--- src/main/java/info/sanaebadi/springboottest/controller/DesignTacoController.java
+++ src/main/java/info/sanaebadi/springboottest/controller/DesignTacoController.java
@@ -1,46 +1,47 @@
 package info.sanaebadi.springboottest.controller;
 
 import info.sanaebadi.springboottest.model.Ingredient;
+import info.sanaebadi.springboottest.model.Ingredient.Type;
 import info.sanaebadi.springboottest.model.Taco;
 import info.sanaebadi.springboottest.model.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
+
 @Controller
 @RequestMapping("/design")
-@SessionAttributes("tacoOrder")
+@SessionAttributes("order")
+@Slf4j
 public class DesignTacoController {
     @ModelAttribute
-    public void addIngrediendToModel(Model model) {
-        List<Ingredient> ingredientList = Arrays.asList(
-                new Ingredient("FLTO", "Flour tortilla", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "Corn tORTILLA", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "ground Beef", Ingredient.Type.PROTEIN),
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
                 new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "Deced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
                 new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
                 new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monerry jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Slasa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour cream", Ingredient.Type.SAUCE)
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
         );
+
         Ingredient.Type[] types = Ingredient.Type.values();
         for (Ingredient.Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredientList, type));
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
     }
 
-    @ModelAttribute(name = "tacoOrder")
+    @ModelAttribute(name = "order")
     public TacoOrder order() {
         return new TacoOrder();
     }
@@ -55,8 +56,16 @@ public class DesignTacoController {
         return "design";
     }
 
-    private Iterable<Ingredient> filterByType(List<Ingredient> ingredientList, Ingredient.Type type) {
-        return ingredientList
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
